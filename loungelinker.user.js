8e573e9e2bb884bc1c2fa392de6454b257bfb7c5
BL van den Aakster <bas.van.den.aakster@hva.nl>
2014-11-15 20:49:16 +0100

    Fixed LoungeAssistant bug

--- loungelinker.user.js
+++ loungelinker.user.js
@@ -6,6 +6,9 @@
 // @author       Basti
 // @match        http://csgolounge.com/
 // @grant        GM_xmlhttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @run-at       document-body
 // @downloadURL  https://raw.githubusercontent.com/basvdaakster/loungelinker/master/loungelinker.user.js
 // @updateURL    https://raw.githubusercontent.com/basvdaakster/loungelinker/master/loungelinker.user.js
 // ==/UserScript==
@@ -22,6 +25,12 @@ var hltvRegex = new RegExp('<a href="/match/([\\d\\w\\-\\.\\?]*?)">\\s*<img src=
 var hltvMatches = [];
 
 function getRedditLinks(matchUrl, callback) {
+	var cache = GM_getValue(matchUrl + '_reddit');
+	if(cache) {
+		console.log('Loaded \'' + matchUrl + '\' from cache');
+		return callback(JSON.parse(cache));
+	}
+
 	var url = 'http://www.reddit.com/r/csgobetting/search?q=selftext:' + encodeURIComponent(matchUrl) + '&sort=top&restrict_sr=on&t=all';
 	GM_xmlhttpRequest ({ method: 'GET', url: url, onload: function (response) {
 		var matches;
@@ -31,11 +40,19 @@ function getRedditLinks(matchUrl, callback) {
 				links.push({ url: 'http://www.reddit.com/r/csgobetting/comments/' + matches[1], text: matches[2] });
 			}
 		}
+		GM_setValue(matchUrl + '_reddit', JSON.stringify(links));
+		console.log('Cached \'' + matchUrl + '\'');
 		callback(links);
 	}});
 }
 
 function getHltvLink(teamA, teamB, callback) {
+	var cache = GM_getValue(teamA + '_' + teamB + '_hltv');
+	if(cache) {
+		console.log('Loaded \'' + teamA + '_' + teamB + '\' from cache');
+		return callback(cache);
+	}
+	
 	teamA = teamA.toLowerCase();
 	teamA = hltvMapping[teamA] || teamB;
 	teamB = teamB.toLowerCase();
@@ -44,7 +61,10 @@ function getHltvLink(teamA, teamB, callback) {
 	if(hltvMatches.length > 0) {
 		for(var i = 0; i < hltvMatches.length; i++) {
 			if(hltvMatches[i].indexOf(teamA) != -1 && hltvMatches[i].indexOf(teamB) != -1) {
-				return callback('http://www.hltv.org/match/' + hltvMatches[i][2]);
+				var link = 'http://www.hltv.org/match/' + hltvMatches[i][2];
+				GM_setValue(teamA + '_' + teamB + '_reddit', link);
+				console.log('Cached \'' + teamA + '_' + teamB + '\'');
+				return callback(link);
 			}
 		}
 		callback(null);
@@ -73,7 +93,7 @@ function addLinks() {
 		var matchLeft = me.find('.matchleft');
 		
 		var container = $('<div style="font-size: 75%; color: black"></div>');
-		matchLeft.append(container);
+		matchLeft.find('a').append(container);
 		
 		var matchUrl = 'http://csgolounge.com/' + me.find('.matchleft a').attr('href');
 		var teams = [ matchLeft.find('.teamtext b')[0].innerHTML.trim(), matchLeft.find('.teamtext b')[1].innerHTML.trim() ];
@@ -97,7 +117,7 @@ function addLinks() {
 				redditContainer.append(header);
 				
 				for(var i = 0; i < links.length; i++) {
-					var a = $('<a href="' + links[i].url + '" style="width: 100%; float: left">' + links[i].text + '</a>');
+					var a = $('<span onclick="window.open(\'' + links[i].url + '\', \'_blank\');return false" style="width: 100%; float: left">' + links[i].text + '</span>');
 					redditContainer.append(a);
 				}
 			}
@@ -110,7 +130,7 @@ function addLinks() {
 			if(link) {
 				var header = $('<strong style="width: 100%; color: #888888; font-weight: bold">HLTV:</strong>');
 				hltvContainer.append(header);
-				var a = $('<a href="' + link + '" style="width: 100%; float: left">' + link + '</a>');
+				var a = $('<span onclick="window.open(\'' + link + '\', \'_blank\');return false" style="width: 100%; float: left">' + link + '</span>');
 				hltvContainer.append(a);
 			}
 			
